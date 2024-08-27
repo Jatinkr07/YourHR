91ffee2359180adfab3316381854ac88fff7b83d
Jatinkr07 <jatinkumar07911@gmail.com>
2024-08-27 14:53:29 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://yourhr-signupv1.onrender.com",
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type",
 };
