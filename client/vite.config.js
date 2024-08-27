0d164c01bf4a724c3cfd7d28f101ef076d38c5cf
Jatinkr07 <jatinkumar07911@gmail.com>
2024-08-27 14:49:38 +0530

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable no-undef */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 5173, // Use the port provided by Render or fallback to 5173 locally
+    host: true, // Expose the server to the network
+  },
+});
